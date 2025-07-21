8bae285d07bc45d89fd3889d88ac58f76d8c5c6a
nasirmars0807 <nasirmars0807@gmail.com>
2025-07-21 16:07:43 -0400

    Update partners-share.py

--- partners-share.py
+++ partners-share.py
@@ -3,15 +3,15 @@
 # F会依照计划，每个月低更新一次，如果有转账需要及时更新。
 # F不需要公开买入的公司，但是向合伙人需要公开账单以便查账。
 # 状态：未启用
-# 最后一次更新时间： 2025年7月20日
+# 最后一次更新时间： 2025年7月21日
 # 预计下一次更新时间： 2025年7月31日
 
 from decimal import Decimal, ROUND_FLOOR
 
 F_start = Decimal('10600.0')    # F先生的之前投入资金（最后更新时间：2025年7月20日）
 L_start = Decimal('1000.0')        # L先生的之前投入资金（最后更新时间：2025年7月20日）
-Z_start = Decimal('1000.0')        # Z先生的之前投入资金（最后更新时间：2025年7月20日）
-total_end = Decimal('10600.0')  # 结算资金（投资后）（最后更新时间：未启用）
+Z_start = Decimal('2000.0')        # Z先生的之前投入资金（最后更新时间：2025年7月20日）
+total_end = Decimal('13600.0')  # 结算资金（投资后）（最后更新时间：未启用）
 
 # Calculate total starting capital
 total_start = F_start + L_start + Z_start
